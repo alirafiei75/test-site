3d404374538b908658ff5489f3abe6a4918add66
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-07-05 16:00:17 +0430

    category field and previous post

--- blog/views.py
+++ blog/views.py
@@ -4,19 +4,35 @@ from django.utils import timezone
 
 
 def blog_view(request):
+    # determining which post to show based on publish date
     now = timezone.now()
     posts = Post.objects.filter(published_date__lte=now)
+    # setting status for published posts
     for post in posts:
         post.status = True
         post.save()
+
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 
 def single_view(request, pid):
+    # determining which posts to show in single page
     posts = Post.objects.filter(status=True)
     post = get_object_or_404(posts,pk=pid)
+    # counted view adding method
     post.counted_views += 1
     post.save()
-    context = {'post':post}
+    # previous and next post
+    try:
+        previous_post = Post.objects.get(status=True, pk = pid - 1)
+    except Post.DoesNotExist:
+        previous_post = None
+        
+    try:
+        next_post = Post.objects.get(status=True, pk = pid + 1)
+    except Post.DoesNotExist:
+        next_post = None
+
+    context = {'post':post, 'previous':previous_post, 'next':next_post}
     return render(request, 'blog/blog-single.html', context)
 
